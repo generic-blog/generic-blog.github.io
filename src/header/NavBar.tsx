9ffaa82841877e5045ef22979a163871c52b67b6
[Cursors] <cursors.dev@gmail.com>
2020-10-06 13:53:28 -0500

    :) this is for pandaboi

---
+++ src/header/NavBar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./navbar.css";
+
+export class NavBar extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <nav className="nav">
+          <div className="logo-container">
+            <h1 className="logo">The Archive</h1>
+          </div>
+          <div className="links">
+            <div className="link-container">
+              <a href="#lorum" className="link">
+                Lorum
+              </a>
+            </div>
+            <div className="link-container">
+              <a href="#ipsum" className="link">
+                Ipsum
+              </a>
+            </div>
+            <div className="link-container">
+              <a href="#dolor" className="link">
+                Dolor
+              </a>
+            </div>
+            <div className="link-container">
+              <a href="#sit" className="link">
+                Sit
+              </a>
+            </div>
+            <div className="link-container">
+              <a href="#amet" className="link">
+                Amet
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
