74a5b2f9eda6a50818c163f85b7b29de3bcc5fb8
[Cursors] <cursors.dev@gmail.com>
2020-10-06 21:17:09 -0500

    backend sucks

---
+++ server/routes/auth.ts
@@ -0,0 +1,32 @@
+const authExpress = require("express");
+const authRouter = authExpress.Router();
+
+authRouter.get("/", (req, res) => {
+  res.json({
+    msg: "Authorized",
+    status: 100,
+  });
+});
+
+authRouter.get("/failure", (req, res) => {
+  res.json({
+    msg: "Failure",
+    status: 400,
+  });
+});
+
+authRouter.get("/unauthorized", (req, res) => {
+  res.json({
+    msg: "Unauthorized",
+    status: 401,
+  });
+});
+
+authRouter.get("/success", (req, res) => {
+  res.json({
+    msg: "Success",
+    status: 200,
+  });
+});
+
+module.exports = authRouter;
