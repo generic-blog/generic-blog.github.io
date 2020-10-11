174bdf505deeedb1c0b6181e570d2100633fe79a
[Cursors] <cursors.dev@gmail.com>
2020-10-11 14:00:17 -0500

    oooh

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -8,15 +8,18 @@ export default class Landing extends React.PureComponent {
       <>
         <section className="landing">
           <main className="main-landing">
-            <h2 className="title">Lorum Ipsum Dolor</h2>
+            <h2 className="title">A Database for Your Server</h2>
+            <p className="subtitle">
+              Store information and have everything at your fingertips
+            </p>
             <hr className="line" />
             <br />
             <div className="buttons">
               <a href="#1" className="landinglink">
-                <div className="btn">Sit</div>
+                <div className="btn">Invite</div>
               </a>
               <a href="#2" className="landinglink">
-                <div className="btn">Amet</div>
+                <div className="btn">Discord</div>
               </a>
             </div>
           </main>
--- src/sections/Section.tsx
+++ src/sections/Section.tsx
@@ -36,25 +36,12 @@ export default class Section extends React.Component<
         ref={this.domRef}
       >
         <main className="container">
-          {this.props.flipped ? (
-            <>
-              <div className="text-container">
-                <p className="text">{text}</p>
-              </div>
-              <div className="image-container">
-                <img src={img.src} alt={img.alt} className="image" />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="image-container">
-                <img src={img.src} alt={img.alt} className="image" />
-              </div>
-              <div className="text-container">
-                <p className="text">{text}</p>
-              </div>
-            </>
-          )}
+          <div className="text-container">
+            <p className="text">{text}</p>
+          </div>
+          <div className="image-container">
+            <img src={img.src} alt={img.alt} className="image" />
+          </div>
         </main>
       </section>
     );
